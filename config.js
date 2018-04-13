85ed72bfc0cea312c3ba90cad5d757cfb2c5f762
Kamil Wojtczak <kamil.wojtczak1@gmail.com>
2018-04-13 16:49:19 +0200

    Add files

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '41u3flvl2btgibccqsqd197dn87r1no2.apps.googleusercontent.com66444113234',
+    'GOOGLE_CLIENT_SECRET':  '0mQjl7ViEX1u_T9_JeHB--Ip-',
+    'CALLBACK_URL': 'http://localhost:3000/google/callback'
+};
